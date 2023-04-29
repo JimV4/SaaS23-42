afd7e4dcfd267714b2958a73c4f2cf8dbd8eb793
Vassiliki M <el19123@mail.ntua.gr>
2023-04-29 22:49:18 +0300

    fixed all checks in line-chart

--- microservice04/bin/routes/diagramRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const diagramController = require("../controllers/diagramController");
-
-const router = express.Router();
-
-router.get("/create", diagramController.createDiagram);
-
-module.exports = router;
