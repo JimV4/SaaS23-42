3e79f610d6c9cded9950e21aff3d8363749d925a
Vassiliki M <el19123@mail.ntua.gr>
2023-05-09 14:38:36 +0300

    fixed bug

--- microservice01/app.js
+++ microservice01/app.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
 });
 
 app.use("/api/myCharts/diagrams", diagramRouter);
