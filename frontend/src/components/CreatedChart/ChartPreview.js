37ac60fc4da178ad2e5ee52b0c0a3d7bea948bfb
ntua-el19830 <el19830@mail.ntua.gr>
2023-05-11 13:26:18 +0300

    Dynamically imported created diagram

--- frontend/src/components/CreatedChart/ChartPreview.js
+++ frontend/src/components/CreatedChart/ChartPreview.js
@@ -1,10 +1,33 @@
 import classes from "./ChartPreview.module.css";
+import { useEffect, useState } from "react";
 
 function ChartPreview(props) {
   console.log(props.imgPath);
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    async function loadImage() {
+      const image = await import(`../../assets/charts/${props.imgPath}`);
+      setImage(image.default);
+    }
+
+    loadImage();
+  }, []);
+
   return (
     <div className={classes["chart-preview"]}>
-      <img src={`../../../../${props.imgPath}`} alt="Your Created Chart" />
+      {image && (
+        <img
+          className={classes.image}
+          // src="../../assets/charts/multi-axis-line-chart/multi-axis-line-chart_dhmhtrhs.vassiliou_1683718427914.png"
+          // src="../../assets/charts/multi-axis-line-chart/bubble.png"
+          // src="../../assets/multi-axis-line-chart_dhmhtrhs.vassiliou_1683717606331.png"
+          // src={`../../assets${props.imgPath}`}
+          // src={image}
+          src={image}
+          alt="Your Created Chart"
+        />
+      )}
     </div>
   );
 }
--- frontend/src/pages/CreatedChartPage.js
+++ frontend/src/pages/CreatedChartPage.js
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 function CreatedChartPage() {
   const state = useLocation();
   const imgPath = state.state.imgPath;
+
   return (
     <div className={classes["container"]}>
       <p>Your selected chart is ready!</p>
