4b6d08e7c6753815a9660ba5e54dc439e3a8da2a
Vassiliki M <el19123@mail.ntua.gr>
2023-05-13 14:47:21 +0300

    added script to run all microservices

---
+++ run_server.bat
@@ -0,0 +1,10 @@
+@echo off
+
+for /l %%i in (1,1,11) do (
+  setlocal enabledelayedexpansion
+  set "dir=microservice%%i"
+  if %%i lss 10 set "dir=microservice0%%i"
+  if exist !dir! (
+    start cmd /c "cd !dir! && npm start"
+  )
+)
