7426d89182989263f9662cc9436c47202be35723
ntua-el19830 <el19830@mail.ntua.gr>
2023-05-17 14:21:53 +0300

    dockers...

--- frontend/src/components/UI/Container.js
+++ frontend/src/components/UI/Container.js
@@ -22,7 +22,7 @@ function Container() {
   async function loginHandler(UserInfo) {
     try {
       const response = await fetch(
-        "http://127.0.0.1:8000/api/myCharts/auth/google/callback",
+        "http://microservice01:8000/api/myCharts/auth/google/callback",
         {
           method: "POST",
           body: JSON.stringify({
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import ChartItemsList from "../components/Charts/ChartItemsList";
 import Container from "../components/UI/Container";
 import { useState } from "react";
 import ConfirmLogin from "../components/Login/ConfirmLogin";
+import axios from "axios";
 
 /* function HomePage() {
   const [confirmLogin, setConfirmLogin] = useState(false);
@@ -18,9 +19,26 @@ import ConfirmLogin from "../components/Login/ConfirmLogin";
   );
 } */
 
+async function letssee() {
+  const myurl = process.env.REACT_APP_URL;
+  console.log(myurl);
+  try {
+    console.log("here");
+
+    const response = await axios.get(`${myurl}`);
+    console.log("here");
+    console.log(response);
+    const jsonresponse = await response.json();
+    console.log(jsonresponse);
+  } catch (error) {
+    console.log("error");
+  }
+}
+
 function HomePage() {
   return (
     <>
+      <button onClick={letssee}>click here!</button>
       <ChartItemsList />
       <Container />
     </>
