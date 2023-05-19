1e3ceff1193d37fbd32e9d5cb92d07bc844e883a
Vassiliki M <el19123@mail.ntua.gr>
2023-05-20 02:06:05 +0300

    fixed bug

--- microservice11/app.js
+++ microservice11/app.js
@@ -8,7 +8,8 @@ const storedChartsRouter = require("./routes/chartsRoutes");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
