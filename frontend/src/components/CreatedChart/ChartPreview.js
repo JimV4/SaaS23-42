55775514ecb1915815cf2a95de1057ba9fc02096
ntua-el19830 <el19830@mail.ntua.gr>
2023-05-20 00:03:50 +0300

    refactor

--- frontend/src/components/CreatedChart/ChartPreview.js
+++ frontend/src/components/CreatedChart/ChartPreview.js
@@ -2,21 +2,6 @@ import classes from "./ChartPreview.module.css";
 import { useEffect, useState } from "react";
 
 function ChartPreview(props) {
-  console.log(props.imgPath);
-
-  const imgPath = props.imgPath;
-  const parts = imgPath.split("/");
-  let newPath = parts[0];
-
-  let chartTitle = newPath
-    .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-
-  if (!chartTitle.includes("Chart")) {
-    chartTitle = chartTitle + " Chart";
-  }
-
   const [image, setImage] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -24,7 +9,7 @@ function ChartPreview(props) {
     setIsLoading(!isLoading);
   }
 
-  useEffect(() => {
+  /* useEffect(() => {
     async function loadImage() {
       const image = await import(`../../assets/charts/${props.imgPath}`);
       handleIsLoading();
@@ -32,16 +17,20 @@ function ChartPreview(props) {
     }
 
     loadImage();
-  }, [props.imgPath]);
+  }, [props.imgPath]); */
 
   return (
     <>
       {isLoading && <p>Loading...</p>}
-      {!isLoading && <p>{`Your ${chartTitle} is ready!`}</p>}
+      {/* {!isLoading && <p>{`Your ${chartTitle} is ready!`}</p>} */}
 
       <div className={classes["chart-preview"]}>
         {image && (
-          <img className={classes.image} src={image} alt="Your Created Chart" />
+          <img
+            className={classes.image}
+            src={props.image}
+            alt="Your Created Chart"
+          />
         )}
       </div>
     </>
