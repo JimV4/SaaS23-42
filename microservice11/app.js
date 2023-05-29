b3f168da3d5e30591215a8ceaa2f2b7433af191d
Vassiliki M <el19123@mail.ntua.gr>
2023-05-29 23:04:09 +0300

    added headers

--- microservice11/app.js
+++ microservice11/app.js
@@ -18,6 +18,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/stored-charts", storedChartsRouter);
 
 module.exports = app;
