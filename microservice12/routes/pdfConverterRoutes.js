b5a823492f68526924c48389f66fe5fd47eed0fa
Vassiliki M <el19123@mail.ntua.gr>
2023-05-29 22:35:01 +0300

    changed to post

--- microservice12/routes/pdfConverterRoutes.js
+++ microservice12/routes/pdfConverterRoutes.js
@@ -5,7 +5,7 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get(
+router.post(
   "/download",
   authController.protect,
   getPNGController.getPNG,
