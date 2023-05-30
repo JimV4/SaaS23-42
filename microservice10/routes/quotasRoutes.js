e43a95da8df6c0877985f708fc7824995c0064dd
Vassiliki M <el19123@mail.ntua.gr>
2023-05-30 11:49:19 +0300

    added purchase quotas functionality

--- microservice10/routes/quotasRoutes.js
+++ microservice10/routes/quotasRoutes.js
@@ -10,6 +10,8 @@ router.get("/check", getQuotasController.checkNumQuotas);
 
 router.patch("/sub", updateQuotasController.subQuotas);
 
+router.patch("/add", updateQuotasController.addQuotas);
+
 router.get("/num-quotas", getQuotasController.getNumQuotas);
 
 module.exports = router;
