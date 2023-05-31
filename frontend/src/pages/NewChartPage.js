99b9bc0605c9afff621fc9c747423153928f3cee
ntua-el19830 <el19830@mail.ntua.gr>
2023-06-01 00:13:29 +0300

    added logo to a page

--- frontend/src/pages/NewChartPage.js
+++ frontend/src/pages/NewChartPage.js
@@ -1,13 +1,17 @@
 import Demos from "../components/NewChart/Demos";
 import classes from "./NewChartPage.module.css";
 import UploadForm from "../components/NewChart/UploadForm";
+import MyChartsLogoV from "../components/UI/MyChartsLogoV";
 
 function NewChartPage() {
   return (
-    <div className={classes.container}>
-      <p>Let's create your own chart!</p>
-      <Demos />
-      <UploadForm />
+    <div className={classes.general}>
+      <MyChartsLogoV />
+      <div className={classes.container}>
+        <p>Let's create your own chart!</p>
+        <Demos />
+        <UploadForm />
+      </div>
     </div>
   );
 }
