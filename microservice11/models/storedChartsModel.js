de00e1613687b9f6e6b17c9ce2d480c1d7ac7c20
IoannisG <el18918@mail.ntua.gr>
2023-06-27 00:30:41 +0300

    [ms11] storedChartsModel bug fix
    imageURL shouldn't be unique

--- microservice11/models/storedChartsModel.js
+++ microservice11/models/storedChartsModel.js
@@ -12,7 +12,6 @@ const storedChartsSchema = new mongoose.Schema({
         imageURL: {
           type: String,
           required: [true, "The imageURL is needed."],
-          unique: true,
         },
         title: {
           type: String,
