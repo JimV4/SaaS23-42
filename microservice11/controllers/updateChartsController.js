48b193fcd7aa5fd01ef13def26887c69e365994e
IoannisG <el18918@mail.ntua.gr>
2023-06-27 00:02:25 +0300

    [ms11] createUser function completed
    Filled createUser function in updateChartsController.js

--- microservice11/controllers/updateChartsController.js
+++ microservice11/controllers/updateChartsController.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const StoredCharts = require(`${__dirname}/../models/storedChartsModel`);
 
 /**
- * Creates a user in the database so he can later store charts.
+ * Creates a user in the database so he/she can later store charts.
  * @param {JSON} req - JSON object containing a body with the user's email.
  * @param {JSON} res - JSON obejct containing a confirmation/rejection of the request.
  * @param {function} next - Pointer to the next function in the middleware stack.
@@ -14,7 +14,26 @@ const StoredCharts = require(`${__dirname}/../models/storedChartsModel`);
  * URL: {baseURL}/stored-charts/create
  */
 exports.createUser = async (req, res, next) => {
-  next();
+  try {
+    if (!req.body.email) {
+      return res.status(400).json({
+        status: "failed",
+        message: "Please provide the email!",
+      });
+    }
+
+    await StoredCharts.create({ email: req.body.email });
+
+    return res.status(200).json({
+      status: "success",
+      message: "The user was successfully stored in the DB with no charts.",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
 
 /**
