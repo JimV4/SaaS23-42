ae0c686a24067fde8304af36b6a53ae6b10fd79f
IoannisG <el18918@mail.ntua.gr>
2023-06-26 21:31:59 +0300

    [ms11] storedChartsModel completed.

---
+++ microservice11/models/storedChartsModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const storedChartsSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: [true, "The email is needed here."],
+    required: true,
+  },
+  storedCharts: {
+    type: [
+      {
+        imageURL: {
+          type: String,
+          required: [true, "The imageURL is needed."],
+          unique: true,
+        },
+        title: {
+          type: String,
+          required: [true, "The title of the chart is needed."],
+        },
+        type: {
+          type: String,
+          required: [true, "The type of the chart is needed."],
+        },
+        createdOn: {
+          type: Date,
+          default: Date.now(),
+        },
+      },
+    ],
+    default: [],
+  },
+});
+
+const StoredCharts = mongoose.model("StoredCharts", storedChartsSchema);
+
+module.exports = StoredCharts;
